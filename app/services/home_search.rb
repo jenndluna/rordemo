35e26081f442bbb93799c72187b795a99250be18
Jennifer Luna <jenndluna@msn.com>
2017-07-10 20:00:33 -0700

    api call working

--- app/services/home_search.rb
+++ app/services/home_search.rb
@@ -8,15 +8,15 @@ class HomeSearchService
     end
     
     def search
-        puts "hello"
-    #     uri = URI(url)
-    #     http = Net::HTTP.new(uri.host, uri.port)
-    #     http.use_ssl = true
+        uri = URI("https://search.onboard-apis.com/propertyapi/v1.0.0/allevents/detail?address1=468%20SEQUOIA%20DR&address2=SMYRNA%2C%20DE")
+        http = Net::HTTP.new(uri.host, uri.port)
 
-    #     request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
+        request = Net::HTTP::Get.new(uri.path, {'Content-Type' => 'application/json'})
+        request.add_field 'apikey', '9d078487e223b1c4d54c3f3a3f628803'
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE # Sets the HTTPS verify mode
 
-    #     request.body = {} # SOME JSON DATA
-
-    #     response = http.request(request)
+        response = http.request(request)
+        return response
     end
 end
